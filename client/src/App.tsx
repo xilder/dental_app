e9998904faa42d6ed5d6b51c6127b0e7664f26d6
xilder <olabowaleayobami@gmail.com>
2024-12-08 00:56:53 +0100

    Refactor (Navigation): Login and Register Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material";
-import About from "./pages/about/About";
-import Home from "./pages/home/Home";
-import Layout from "./pages/Layout";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import About from "./pages/about/About";
+import HomeLayout from './pages/home/HomeLayout';
+// import Patient from './pages/patient/Patient';
+import AccountsPage from './pages/login/AccountsPage';
+// import Layout from "./pages/Layout";
+import './App.css';
+import Home from './pages/home/Home';
 
 const App = () => {
-  const theme = createTheme();
   return (
     <Router>
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
+      <Routes>
+        <Route path='/' element={<HomeLayout />}>
+          <Route index element={<Home />} />
+          {/* <Route path='/patient' element={<Patient />} /> */}
+          <Route path='accounts' element={<AccountsPage />} />
+        </Route>
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
     </Router>
   );
 };
