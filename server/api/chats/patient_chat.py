a6532d6b007576a20afc7f700715c3d43071a495
xilder <olabowaleayobami@gmail.com>
2025-01-23 15:39:59 +0100

    Feat (routes): routes to query resources for the front end

--- server/api/chats/patient_chat.py
+++ server/api/chats/patient_chat.py
@@ -1,51 +1,77 @@
-from flask import session
-from flask_socketio import Namespace, join_room, leave_room, disconnect
+from flask import session, request
+from flask_socketio import Namespace, emit, join_room, leave_room, disconnect
 from models.messages import Message
 from models.engine.db import db_client
 
+online_users: dict[str, str] = {}
+
+
 class Chat(Namespace):
     def on_connect(self):
-        if not session['user_id']:
+        if not session["user_id"]:
             disconnect()
-        print("Client connected to namespace")
+        print(f"Client connected: {request.sid}")
+        user = session["user_id"]
+        online_users[user] = request.sid
+        o_users = [a_user for a_user in online_users.keys() if a_user != user]
+        emit('users', o_users, broadcast=True)
+
+        # offline_msgs = db_client.find_obj_by('message', receiver_id=user, type='pending').all()
+
+        # for msg in offline_msgs:
+        #     sender_id = msg.to_json().get('sender_id')
+        #     text = msg.get('text')
+        #     created_at = msg.get('created_at')
+        #     self.emit('offline_message', {'sender_id': sender_id, 'text': text, 'created_at': created_at}, room=request.sid)
+        #     db_client.update('message', msg.id, type='delivered')
+        # print("Client connected to namespace")
 
     def on_disconnect(self):
+        user = session["user_id"]
+        del online_users[user]
         disconnect()
 
-    def on_my_event(self, data):
-        result = 1
-        for i in data:
-            result *= i
-        self.emit('response', result)
-    def on_hello(self, data):
-        print("Received data:", data)
-        # Emit an event to all clients in this namespace
-        result = 1
-        print(data)
-        for i in data:
-            result = result * i
-        self.emit('response', {'message': result})
+    # def on_join_room(self, data):
+    #     patient = data['patient']
+    #     doctor = data['doctor']
+    #     join_room(f'room_{patient}_{doctor}')
 
-    def on_patient_join_chat_room(self, data):
-        patient = session['user']
-        doctor_id = data['doctor_id']
-        chat_room = f'room_{patient.id}_{doctor_id}'
-        join_room(chat_room)
-        self.emit('joined_room', f'{patient.first_name} joined room', room=chat_room)
+    # def on_message(self, data):
+    #     # room_id = data['room_id']
+    #     sender_id = session["sender_id"]
+    #     receiver_id = data["receiver_id"]
+    #     text = data["text"]
+    #     message = db_client.add(
+    #         "message", sender_id=sender_id, receiver_id=receiver_id, text=text
+    #     )
+    #     recipient_sid = online_users.get(receiver_id, None)
 
-    def on_doctor_join_chat_room(self, data):
-        doctor = session['user']
-        patient_id = data['patient_id']
-        chat_room = f'room_{patient_id}_{doctor.id}'
-        join_room(chat_room)
-        self.emit('joined_room', f'{doctor.first_name} joined room', room=chat_room)
+    #     if recipient_sid and message:
+    #         data["created_at"] = message.to_json().created_at
+    #         self.emit("message", data, room=recipient_sid)
+    #     else:
+    #         db_client.update("message", message.id, type="pending")
 
-    def message(self, data):
-        sender_id = data['sender_id']
-        receiver_id = data['receiver_id']
-        room_id = data['room_id']
-        text = data['text']
-        message = Message(sender_id=sender_id, receiver_id=receiver_id, text=text, room_id=room_id)
-        db_client.add(message)
-        self.emit('message', text, room=room_id)
+    # def message(self, data):
+    #     sender_id = data['sender_id']
+    #     receiver_id = data['receiver_id']
+    #     room_id = data['room_id']
+    #     text = data['text']
+    #     message = Message(sender_id=sender_id, receiver_id=receiver_id, text=text, room_id=room_id)
+    #     db_client.add(message)
+    #     self.emit('message', text, room=room_id)
 
+    def on_users(self, data):
+        if data:
+            print(data)
+        # user = session["user_id"]
+        o_users = [user for user in online_users.keys()]
+        print(online_users, o_users)
+        emit('users', o_users, broadcast=True)
+
+    def on_send_message(self, data):
+        receiver_id = data["receiver_id"]
+        recipient_sid = online_users.get(receiver_id, None)
+        print(recipient_sid, data)
+
+        self.emit("receive_message", data, room=recipient_sid)
--- server/api/views/auth_view.py
+++ server/api/views/auth_view.py
@@ -2,132 +2,136 @@ from flask import abort, jsonify, make_response, request, session
 from itsdangerous import SignatureExpired
 from api.views import app_view
 from models.engine.auth import Auth
-from models.user import User
 from errors.account_error import UnconfirmedAccountError
 
 AUTH = Auth()
 
 
-@app_view.route("/register", methods=["POST"])
-def register():
-    from tasks.accounts_task import send_verification_message
-
+@app_view.route("/register/<role>", methods=["POST"])
+def register(role: str):
     """registration route"""
     if not request.get_json():
         return make_response(jsonify("Not a JSON"), 400)
-
     try:
         user_params = request.get_json()
-        user = AUTH.register_user(**user_params)
-        token = AUTH.get_token(user.email)
-
+        user = AUTH.register_user(cls=role, **user_params)
+        if not user:
+            return make_response("Registration failed", 400)
+        token = AUTH.get_reset_token(cls=role, email=str(user.email))
         if token:
+            from tasks.accounts_task import send_verification_message
+
             send_verification_message(user)
         return make_response(
             f"A confirmation mail has been sent to {user.email}.",
             201,
         )
     except Exception as e:
-        AUTH.delete_user(user.id)
-        return make_response(str(e), 400)
+        # AUTH.delete_user(user.id)
+        return make_response(str(e), 401)
 
 
-@app_view.route("/confirmation/<token>")
-def confirmation(token):
+@app_view.route("/confirmation/<role>/<token>")
+def confirmation(role: str, token: str):
     try:
         if not token:
             abort(404)
-        user = AUTH.get_user_from_token(token=token)
-        if user.confirmed:
-            return make_response("Account already confirmed", 403)
-        if AUTH.update(user.id, confirmed=True):
+        user = AUTH.get_user_from_token(cls=role, token=token)
+        if not user:
+            return make_response("Invalid token", 403)
+        if bool(user.confirmed):
+            return make_response(
+                "Account already confirmed. Please proceed to login", 400
+            )
+        if AUTH.update(cls=role, id=user.id, confirmed=True):
             return make_response("Account Confirmation Successful", 200)
+        return make_response("Account confirmation failed", 400)
     except SignatureExpired:
-        return make_response('Account expired. Please your email for a new confirmation mail')
+        return make_response(
+            "Token expired. Please your email for a new confirmation mail",
+            400,
+        )
     except Exception:
         return make_response("Invalid token", 403)
 
 
-@app_view.route("/login", methods=["POST"])
+@app_view.route("/login/", methods=["POST"])
 def login():
     """login route"""
     try:
         if request.get_json() is None:
             return make_response(jsonify("Not a json"), 401)
-
         user_params = request.get_json()
         data = user_params.get("data", None)
         password = user_params.get("password", None)
-
         user = AUTH.valid_login(email=data, username=data, password=password)
         if not user:
             return make_response("Invalid login credentials", 404)
-
+        # TODO: add check for account confirmation
+        session_user = user.to_json()
+        for key in ["updated_at", "created_at", "type", "session", "token"]:
+            del session_user[key]
         session["user_id"] = user.id
-        session["user"] = user.to_json()
-        return make_response(jsonify(user.to_json()))
+        session["user"] = session_user
+        return make_response(jsonify(session_user))
     except UnconfirmedAccountError as e:
         return make_response(str(e), 403)
 
 
-
 @app_view.route("/profile", methods=["GET"])
-def profile() -> User:
+def profile():
     """returns the information of the user logged in"""
-    user: User = session.get("user", None)
+    user = session.get("user", None)
     if user is None:
         return make_response("Unauthorised", 401)
-
     return make_response(jsonify(user))
 
 
 @app_view.route("/logout", methods=["DELETE"])
-def logout() -> str:
+def logout():
     """terminates session"""
     session.pop("user_id")
     session.pop("user")
     return make_response(jsonify(), 200)
 
 
-@app_view.route("/users", methods=["DELETE"])
-def delete_user() -> str:
+@app_view.route("/users/", methods=["DELETE"])
+def delete_user():
     """deletes a user"""
-    id = session.get("user_id", None)
+    params = request.get_json()
+    id = params.get("id", None)
     if not id:
         abort(401)
-
     if not AUTH.delete_user(id):
         return make_response(jsonify("Account not deleted"), 403)
-
     return make_response(jsonify("Account deleted"))
 
 
-@app_view.route("/reset_password", methods=["POST"])
-def reset_password():
-    if request.get_json() is None:
-        return make_response(jsonify("Not a json"), 401)
+# @app_view.route("/reset_password", methods=["POST"])
+# def reset_password():
+#     if request.get_json() is None:
+#         return make_response(jsonify("Not a json"), 401)
 
-    user_params = request.get_json()
-    email = user_params.get("email", None)
-    token = AUTH.get_reset_password_token(email=email)
-    if token is None:
-        return make_response(jsonify("Unauthorised"), 401)
-    return make_response(jsonify(f"Visit {email} for the reset email"))
+#     user_params = request.get_json()
+#     email = user_params.get("email", None)
+#     token = AUTH.get_reset_token(cls='user', email=email)
+#     if token is None:
+#         return make_response(jsonify("Unauthorised"), 401)
+#     return make_response(jsonify(f"Visit {email} for the reset email"))
 
 
-@app_view.route("/change_password/<token>", methods=["POST"])
+@app_view.route("/change_password", methods=["POST"])
 def change_password(token):
     """Change Password"""
-    user = AUTH.get_user_from_token(token)
-    if user is None:
-        return make_response(jsonify("Invalid token"), 401)
     user_params = request.get_json()
     password = user_params.get("password", None)
-    if not AUTH.change_password(token, password):
+    id = session.get("user_id", None)
+    user = AUTH.find_user_by(cls="user", id=id)
+    if user is None:
+        return make_response(jsonify("Unauthorised"), 403)
+    if not AUTH.update(cls="user", id=id, password=password):
         return make_response(jsonify("Invalid token"), 401)
     return make_response(jsonify("Password changed"))
-
-    # return make_response(jsonify(message='Change Password'))
 
 
 # @app_view.route('/user', methods=['POST'])
